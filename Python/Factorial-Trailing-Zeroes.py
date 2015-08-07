58f08fda85adfd6cecdafa4393d4194bbac679f2
Andrew9Tech <Andrew9Tech@users.noreply.github.com>
2015-08-07 19:14:48 +0800

    Create Factorial-Trailing-Zeroes.py

---
+++ Python/Factorial-Trailing-Zeroes.py
@@ -0,0 +1,22 @@
+
+# Time:  O(logn)
+# Space: O(1)
+#
+# Given an integer n, return the number of trailing zeroes in n!.
+# 
+# Note: Your solution should be in logarithmic time complexity.
+#
+
+
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def trailingZeroes(self, n):
+        result = 0
+        while n > 0:
+            result += n/5
+            n /= 5
+        return result
+        
+  if __name__ == "__main__":
+    print Solution().trailingZeroes(100)
