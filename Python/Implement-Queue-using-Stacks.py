ce746933fcf59dce387a4706fe2e2adf899b45aa
Andrew9Tech <Andrew9Tech@users.noreply.github.com>
2015-08-08 11:17:37 +0800

    Update Implement-Queue-using-Stacks.py

--- Python/Implement-Queue-using-Stacks.py
+++ Python/Implement-Queue-using-Stacks.py
@@ -12,6 +12,7 @@
 #
 # You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
 # 
+# 以下思路来源： http://bookshadow.com/weblog/2015/07/07/leetcode-implement-queue-using-stacks/
 # 题目大意：
 # 使用栈实现队列的下列操作：
 # 
