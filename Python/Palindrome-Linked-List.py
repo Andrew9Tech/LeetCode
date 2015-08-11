54ce8e4f7da154f23d2773b6e0d39d1779370984
Andrew9Tech <Andrew9Tech@users.noreply.github.com>
2015-08-11 19:17:48 +0800

    Update Palindrome-Linked-List.py

--- Python/Palindrome-Linked-List.py
+++ Python/Palindrome-Linked-List.py
@@ -5,11 +5,12 @@
 # Could you do it in O(n) time and O(1) space?
 
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
 class Solution:
     # @param {ListNode} head
     # @return {boolean}
